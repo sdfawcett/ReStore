9ccfa85ec7f0756e736f741408021b4736a1e487
Shayne Fawcett <shayne.fawcett@gmail.com>
2023-10-25 12:19:24 -0400

    adding CORS config adding product TS interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,34 @@
 import { useEffect, useState } from "react"
+import { Product } from "./product"
 
 function App() {
-  const [products, setProducts] = useState([
-    {name: 'product1', price: 100.00},
-    {name: 'product2', price: 200.00},
-    {name: 'product2', price: 200.00},
-  ]) 
-
-  function addProduct() {
-    setProducts(prevState => [...products, 
-      {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}
-    ])
+  const [products, setProducts] = useState<Product[]>([]) 
 
     useEffect(() => {
       fetch('http://localhost:5001/api/products')
         .then(response => response.json())
         .then(data => setProducts(data))
     }, [])
-  }
+
+    function addProduct() {
+      setProducts(prevState => [...prevState, 
+        {
+          id: prevState.length + 101,
+          name: 'product' + (prevState.length + 1), 
+          price: (prevState.length * 100) + 100,
+          brand: 'some brand',
+          description: 'some description',
+          pictureUrl: 'http://picsum.photos/200',
+        }
+      ])
+    }
 
   return (
     <div>
       <h1>ReStore</h1>
       <ul>
-        {products.map((product, index) => (
-          <li key={index}>{product.name} - {product.price}</li>
+        {products.map(product => (
+          <li key={product.id}>{product.name} - {product.price}</li>
         ))}
       </ul>
       <button onClick={addProduct}>Add product</button>
