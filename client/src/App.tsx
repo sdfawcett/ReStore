f00aa84c9f2b304f95b037ab968310b2667f3eb5
Shayne Fawcett <shayne.fawcett@gmail.com>
2023-10-24 20:47:09 -0400

    initial api call for products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [products, setProducts] = useState([
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00},
+    {name: 'product2', price: 200.00},
+  ]) 
+
+  function addProduct() {
+    setProducts(prevState => [...products, 
+      {name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100}
+    ])
+
+    useEffect(() => {
+      fetch('http://localhost:5001/api/products')
+        .then(response => response.json())
+        .then(data => setProducts(data))
+    }, [])
+  }
+
   return (
     <div>
       <h1>ReStore</h1>
+      <ul>
+        {products.map((product, index) => (
+          <li key={index}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add product</button>
     </div>
   )
 }
