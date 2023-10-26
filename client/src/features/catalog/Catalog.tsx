a289080366bc7e086320c4c7a9411768bb21b8bb
Shayne Fawcett <shayne.fawcett@gmail.com>
2023-10-25 21:13:58 -0400

    added material ui, componentized Catalog, product images added

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
 import { Product } from "../../app/models/product";
 
 interface Props {
@@ -9,12 +10,17 @@ export default function Catalog({products, addProduct}: Props) {
    return (
       <>
          <h1>Catalog</h1>
-         <ul>
+         <List>
          {products.map(product => (
-            <li key={product.id}>{product.name} - {product.price}</li>
+            <ListItem key={product.id}>
+               <ListItemAvatar>
+                  <Avatar src={product.pictureUrl} />
+               </ListItemAvatar>
+               <ListItemText>{product.name} - {product.price}</ListItemText>
+            </ListItem>
          ))}
-         </ul>
-         <button onClick={addProduct}>Add product</button>
+         </List>
+         <Button variant='contained' onClick={addProduct}>Add product</Button>
       </>
    )
 }
