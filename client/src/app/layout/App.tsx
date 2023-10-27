04d9c18015ff3a867aa80abf44e2f1cb4395a78b
Shayne Fawcett <shayne.fawcett@gmail.com>
2023-10-27 15:19:30 -0400

    added switch component and theme change functions/logic/palette values

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -6,19 +6,25 @@ import { useState } from "react"
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light'
-
   const theme = createTheme({
     palette: {
-      mode: paletteType
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
     }
   })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
         <Catalog />
       </Container>
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,20 +1,18 @@
-import { AppBar, FormControlLabel, FormGroup, Switch, Toolbar, Typography } from "@mui/material";
+import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
 
 interface Props {
   darkMode: boolean;
   handleThemeChange: () => void;
 }
 
-export default function Header() {
+export default function Header({ darkMode, handleThemeChange }: Props) {
   return (
     <AppBar position="static" sx={{mb: 4}}>
       <Toolbar>
          <Typography variant="h6">
             ReStore
          </Typography>
-         <FormGroup>
-          <FormControlLabel control={<Switch defaultChecked />} label="" />
-        </FormGroup>
+         <Switch checked={darkMode} onChange={handleThemeChange} />
       </Toolbar>
     </AppBar>
   )
