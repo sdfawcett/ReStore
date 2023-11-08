b5ea68ad250fc7f0623bbcd586c15f93c48e00d1
Shayne Fawcett <shayne.fawcett@gmail.com>
2023-11-07 20:38:55 -0500

    initial error handling config

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,35 +1,44 @@
-import { ThemeProvider } from "@emotion/react"
-import Header from "./Header"
-import { Container, CssBaseline, createTheme } from "@mui/material"
-import { useState } from "react"
-import { Outlet } from "react-router-dom"
+import { ThemeProvider } from '@emotion/react'
+import Header from './Header'
+import { Container, CssBaseline, createTheme } from '@mui/material'
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
+	const [darkMode, setDarkMode] = useState(false)
+	const paletteType = darkMode ? 'dark' : 'light'
+	const theme = createTheme({
+		palette: {
+			mode: paletteType,
+			background: {
+				default: paletteType === 'light' ? '#eaeaea' : '#121212',
+			},
+		},
+	})
 
-  const [darkMode, setDarkMode] = useState(false);
-  const paletteType = darkMode ? 'dark' : 'light'
-  const theme = createTheme({
-    palette: {
-      mode: paletteType,
-      background: {
-        default: paletteType === 'light' ? '#eaeaea' : '#121212'
-      }
-    }
-  })
+	function handleThemeChange() {
+		setDarkMode(!darkMode)
+	}
 
-  function handleThemeChange() {
-    setDarkMode(!darkMode);
-  }
-
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-      <Container>
-        <Outlet />
-      </Container>
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider theme={theme}>
+			<ToastContainer
+				position="bottom-right"
+				hideProgressBar
+				theme="colored"
+			/>
+			<CssBaseline />
+			<Header
+				darkMode={darkMode}
+				handleThemeChange={handleThemeChange}
+			/>
+			<Container>
+				<Outlet />
+			</Container>
+		</ThemeProvider>
+	)
 }
 
 export default App
